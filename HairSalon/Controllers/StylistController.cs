684b06196c80d89b39050245fbb77478c2166c7f
Lindsey Baker <lindsey.baker0@gmail.com>
2019-05-10 17:03:33 -0700

    Fix error in Stylist New controller

--- HairSalon/Controllers/StylistController.cs
+++ HairSalon/Controllers/StylistController.cs
@@ -15,5 +15,51 @@ namespace HairSalon.Controllers
       return View(allStylists);
     }
 
+    [HttpGet("/stylist/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpPost("/stylist")]
+    public ActionResult Create(string firstName, string lastName)
+    {
+      Stylist newStylist = new Stylist(firstName, lastName);
+      List<Stylist> allStylists = Stylist.GetAll();
+      newStylist.Save();
+      return RedirectToAction("Index");
+    }
+
+    [HttpGet("/stylist/{id}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Stylist selectedStylist = Stylist.Find(id);
+      List<Client> stylistClients = selectedStylist.GetClients();
+      model.Add("stylist", selectedStylist);
+      model.Add("stylistClients", stylistClients);
+      return View(selectedStylist);
+    }
+
+    [HttpPost("/stylist/{stylistId}/client")]
+    public ActionResult Create(string firstName, string lastName, DateTime clientSince, int stylistId)
+    {
+      Stylist foundStylist = Stylist.Find(stylistId);
+      Client newClient = new Client(firstName, lastName, clientSince, stylistId);
+      newClient.Save();
+      foundStylist.GetClients();
+      return View("Show", foundStylist);
+    }
+
+    [HttpPost("/stylist/{stylistId}/delete")]
+    public ActionResult Delete(int stylistId)
+    {
+      Stylist stylist = Stylist.Find(stylistId);
+      stylist.DeleteStylist();
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      model.Add("stylist", stylist);
+      return RedirectToAction("Index");
+    }
+
   }
 }
