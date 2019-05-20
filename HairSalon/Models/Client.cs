2dd6901ecacf186af227a2ec14c35f0dfad67aa2
Lindsey <lindsey.baker0@gmail.com>
2019-05-19 20:07:22 -0700

    Add DeleteAll function to Clients

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -212,5 +212,19 @@ namespace HairSalon.Models
       }
     }
 
+    public static void DeleteAll()
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"DELETE FROM clients;";
+      cmd.ExecuteNonQuery();
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+    }
+
   }
 }
